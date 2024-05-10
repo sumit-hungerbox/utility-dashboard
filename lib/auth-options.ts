c026359849cd6c4dee5978758889ae91dbbbaf28
Sumit Kumathalli <sumit.kumathalli@hungerbox.com>
2024-05-10 19:26:37 +0530

    fix: removed github

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -1,13 +1,8 @@
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import CredentialProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
     CredentialProvider({
       credentials: {
         email: {
